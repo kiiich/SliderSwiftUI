6be78037ee11b1022ba6342c3f649ce3ccbc1d8a
kiiich <59602632+kiiich@users.noreply.github.com>
2021-11-09 17:27:46 +0300

    Initial Commit

---
+++ Slider/SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderApp.swift
+//  Slider
+//
+//  Created by Николай on 09.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
